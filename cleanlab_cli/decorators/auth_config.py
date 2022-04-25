358057e14152cc1bb463eb92a11596af3f065ca2
calebchiam <calebchiam@gmail.com>
2022-04-25 17:10:52 -0400

    separate decorators into their own dir

---
+++ cleanlab_cli/decorators/auth_config.py
@@ -0,0 +1,23 @@
+import click
+from cleanlab_cli.api_service import validate_api_key
+from cleanlab_cli.click_helpers import abort
+from cleanlab_cli.login import login_helpers as helpers
+
+
+class AuthConfig:
+    def __init__(self):
+        self.api_key = None
+
+    def get_api_key(self):
+        if self.api_key is None:
+            try:
+                api_key = helpers.load_api_key()
+                if api_key is None or not validate_api_key(api_key):
+                    raise ValueError("Invalid API key.")
+                self.api_key = api_key
+            except (FileNotFoundError, KeyError, ValueError):
+                abort("No valid API key found. Run 'cleanlab login' before running this command.")
+        return self.api_key
+
+
+auth_config = click.make_pass_decorator(AuthConfig, ensure=True)
