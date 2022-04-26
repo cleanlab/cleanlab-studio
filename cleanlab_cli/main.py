de3a96340be4e44b0dc32fe27b51f96d19a6d20d
calebchiam <calebchiam@gmail.com>
2022-04-26 17:28:14 -0400

    init settings on main command

--- cleanlab_cli/main.py
+++ cleanlab_cli/main.py
@@ -1,10 +1,12 @@
 import click
 from cleanlab_cli.dataset.commands import dataset
 from cleanlab_cli.login.login import login
+from cleanlab_cli.settings import CleanlabSettings
 
 
 @click.group()
 def cli():
+    CleanlabSettings.init_cleanlab_settings()
     pass
 
 
