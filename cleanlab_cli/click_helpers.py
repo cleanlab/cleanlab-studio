dcc13679ea97ddb3635bf73e74180ae0f08b89af
calebchiam <calebchiam@gmail.com>
2022-04-27 02:13:32 -0400

    refactor prompts for reuse

--- cleanlab_cli/click_helpers.py
+++ cleanlab_cli/click_helpers.py
@@ -8,6 +8,7 @@ Information: default
 Success/completion: green
 """
 import click
+import os
 from click import ClickException, secho, style, echo
 
 
@@ -36,3 +37,22 @@ def info(message):
 
 def log(message):
     echo(message)
+
+
+def prompt_for_filepath(prompt_message, default=None):
+    filepath = click.prompt(prompt_message, default=default)
+    while not os.path.exists(filepath):
+        error(f"No file exists at: {filepath}")
+        filepath = click.prompt(prompt_message)
+    return filepath
+
+
+def prompt_with_optional_default(prompt_message, default=None):
+    if default:
+        retval = click.prompt(
+            f"{prompt_message} Leave this blank to use default",
+            default=default,
+        )
+    else:
+        retval = click.prompt(prompt_message)
+    return retval
