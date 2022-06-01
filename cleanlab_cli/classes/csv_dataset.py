36d513f25a496a670b7a631bf6266450d001ab15
calebchiam <calebchiam@gmail.com>
2022-06-01 18:07:30 -0400

    bug fix: 'charmap' codec can't decode byte 0x9d

--- cleanlab_cli/classes/csv_dataset.py
+++ cleanlab_cli/classes/csv_dataset.py
@@ -6,27 +6,27 @@ from .dataset import Dataset
 
 class CsvDataset(Dataset):
     def count_rows(self) -> int:
-        with open(self.filepath, "r") as f:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
             reader = csv.reader(f)
             return len(list(reader)) - 1  # first row is headers
 
     def get_columns(self):
-        with open(self.filepath, "r") as f:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
             reader = csv.reader(f)
             columns = next(reader)
             return columns
 
     def read_streaming_records(self):
-        with open(self.filepath, "r") as f:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
             reader = csv.reader(f)
             columns = next(reader)
             for row in reader:
                 yield dict(zip(columns, row))
 
     def read_streaming_values(self):
-        with open(self.filepath, "r") as f:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
             reader = csv.reader(f)
-            columns = next(reader)
+            next(reader)
             for row in reader:
                 yield row
 
