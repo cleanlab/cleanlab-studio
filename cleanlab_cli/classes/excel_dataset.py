5552151bec58cfc580931d929fcd18a84ff564c7
calebchiam <calebchiam@gmail.com>
2022-06-04 17:04:10 -0400

    optimize ExcelDataset to get values array

--- cleanlab_cli/classes/excel_dataset.py
+++ cleanlab_cli/classes/excel_dataset.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from .dataset import Dataset
+from cleanlab_cli.classes.dataset import Dataset
 import pyexcel
 import pandas as pd
 
@@ -11,12 +11,14 @@ class ExcelDataset(Dataset):
             yield r
 
     def read_streaming_values(self):
-        for r in pyexcel.iget_records(file_name=self.filepath):
-            # TODO optimize
-            yield r.values()
+        stream = pyexcel.iget_array(file_name=self.filepath)
+        next(stream)  # skip header row
+        for r in stream:
+            yield r
 
     def read_file_as_dataframe(self):
         return pd.read_excel(self.filepath, keep_default_na=True)
 
     def get_columns(self) -> List[str]:
-        return super().get_columns()
+        stream = pyexcel.iget_array(file_name=self.filepath)
+        return next(stream)
