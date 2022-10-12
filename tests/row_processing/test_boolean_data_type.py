a9c03c8b46c4c6dd1b8b5a827fea51c5aef0c5c7
calebchiam <calebchiam@gmail.com>
2022-10-12 18:53:39 +0800

    add float tests

--- tests/row_processing/test_boolean_data_type.py
+++ tests/row_processing/test_boolean_data_type.py
@@ -11,7 +11,7 @@ from tests.row_processing.utils import process_record_with_fields
 
 class TestBooleanDataType:
     @staticmethod
-    def process_with_integer_numeric(
+    def process_with_boolean(
         record: RecordType,
     ) -> Tuple[Optional[RecordType], Optional[str], Optional[RowWarningsType]]:
         return process_record_with_fields(
@@ -22,77 +22,77 @@ class TestBooleanDataType:
         )
 
     def test_boolean_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": True})
+        row, row_id, warnings = self.process_with_boolean({"x": True})
         assert row["x"] is True
 
     def test_yes_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "yes"})
+        row, row_id, warnings = self.process_with_boolean({"x": "yes"})
         assert row["x"] is True
 
     def test_no_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "no"})
+        row, row_id, warnings = self.process_with_boolean({"x": "no"})
         assert row["x"] is False
 
     def test_true_string_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "true"})
+        row, row_id, warnings = self.process_with_boolean({"x": "true"})
         assert row["x"] is True
 
     def test_false_string_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "false"})
+        row, row_id, warnings = self.process_with_boolean({"x": "false"})
         assert row["x"] is False
 
     def test_t_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "t"})
+        row, row_id, warnings = self.process_with_boolean({"x": "t"})
         assert row["x"] is True
 
     def test_f_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "f"})
+        row, row_id, warnings = self.process_with_boolean({"x": "f"})
         assert row["x"] is False
 
     def test_y_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "y"})
+        row, row_id, warnings = self.process_with_boolean({"x": "y"})
         assert row["x"] is True
 
     def test_n_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "n"})
+        row, row_id, warnings = self.process_with_boolean({"x": "n"})
         assert row["x"] is False
 
     def test_1_string_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "1"})
+        row, row_id, warnings = self.process_with_boolean({"x": "1"})
         assert row["x"] is True
 
     def test_0_string_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "0"})
+        row, row_id, warnings = self.process_with_boolean({"x": "0"})
         assert row["x"] is False
 
     def test_1_integer_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": 1})
+        row, row_id, warnings = self.process_with_boolean({"x": 1})
         assert row["x"] is True
 
     def test_0_integer_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": 0})
+        row, row_id, warnings = self.process_with_boolean({"x": 0})
         assert row["x"] is False
 
     def test_invalid_integer_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": 100})
+        row, row_id, warnings = self.process_with_boolean({"x": 100})
         assert row["x"] is None
 
     def test_invalid_string_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": "ok"})
+        row, row_id, warnings = self.process_with_boolean({"x": "ok"})
         assert row["x"] is None
 
     def test_1_float_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": 1.0})
+        row, row_id, warnings = self.process_with_boolean({"x": 1.0})
         assert row["x"] is True
 
     def test_0_float_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": 0.0})
+        row, row_id, warnings = self.process_with_boolean({"x": 0.0})
         assert row["x"] is False
 
     def test_null_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": None})
+        row, row_id, warnings = self.process_with_boolean({"x": None})
         assert row["x"] is None
 
     def test_nan_input(self) -> None:
-        row, row_id, warnings = self.process_with_integer_numeric({"x": float("nan")})
+        row, row_id, warnings = self.process_with_boolean({"x": float("nan")})
         assert row["x"] is None
---
+++ tests/row_processing/test_float_data_type.py
@@ -0,0 +1,58 @@
+from typing import Tuple, Optional
+
+from cleanlab_cli.dataset import (
+    RowWarningsType,
+    DataType,
+    FeatureType,
+)
+from cleanlab_cli.types import RecordType
+from tests.row_processing.utils import process_record_with_fields
+
+
+class TestFloatDataType:
+    @staticmethod
+    def process_with_float(
+        record: RecordType,
+    ) -> Tuple[Optional[RecordType], Optional[str], Optional[RowWarningsType]]:
+        return process_record_with_fields(
+            record=record,
+            fields={
+                "x": dict(data_type=DataType.float.value, feature_type=FeatureType.numeric.value)
+            },
+        )
+
+    # def test_boolean_input(self) -> None:
+    #     row, row_id, warnings = self.process_with_float({"x": True})
+    #     assert row["x"] is None
+
+    def test_float_string_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": "180.0"})
+        assert row["x"] == 180.0
+
+    def test_integer_string_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": "180"})
+        assert row["x"] == 180.0
+
+    def test_invalid_string_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": "cc982"})
+        assert row["x"] is None
+
+    def test_percentage_string_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": "180%"})
+        assert row["x"] == 180.0
+
+    def test_integer_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": 180})
+        assert row["x"] == 180
+
+    def test_float_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": 180.0})
+        assert row["x"] == 180.0
+
+    def test_nan_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": float("nan")})
+        assert row["x"] is None
+
+    def test_null_input(self) -> None:
+        row, row_id, warnings = self.process_with_float({"x": None})
+        assert row["x"] is None
