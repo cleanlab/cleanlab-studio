17d3349257a7bc30db90f296a1f6ae3bb61a475b
Anish Athalye <me@anishathalye.com>
2022-12-20 09:41:26 -0500

    Hacky fix

--- cleanlab_studio/studio/studio.py
+++ cleanlab_studio/studio/studio.py
@@ -9,6 +9,7 @@ from cleanlab_studio.internal.schema import Schema
 from cleanlab_studio.internal.settings import CleanlabSettings
 from cleanlab_studio.studio import upload
 
+
 class Studio:
     _api_key: str
 
@@ -20,10 +21,11 @@ class Studio:
                 if api_key is None:
                     raise ValueError
             except (FileNotFoundError, KeyError, ValueError):
-                raise ValueError("No API key found; either specify API key or log in with 'cleanlab login' first")
+                raise ValueError(
+                    "No API key found; either specify API key or log in with 'cleanlab login' first"
+                )
         api.validate_api_key(api_key)
         self._api_key = api_key
-
 
     def upload_text_dataset(
         self,
@@ -48,7 +50,6 @@ class Studio:
         ds = PandasDataset(dataset.toPandas())
         return upload.upload_tabular_dataset(self._api_key, ds, schema, id)
 
-
     def upload_image_dataset(
         self,
         dataframe,  # spark.DataFrame
@@ -60,10 +61,20 @@ class Studio:
         name: str,
         id: Optional[str] = None  # for resuming upload
     ) -> str:
-        return upload.upload_image_dataset(self._api_key, dataframe, name, id_column, path_column, content_column, label_column, dataset_id=id)
-
+        return upload.upload_image_dataset(
+            self._api_key,
+            dataframe,
+            name,
+            id_column,
+            path_column,
+            content_column,
+            label_column,
+            dataset_id=id,
+        )
 
     def apply_corrections(self, cleanset_id: str, dataset):
+        from pyspark.sql.functions import udf
+
         spark = dataset.sparkSession
         project_id = api.get_project_of_cleanset(self._api_key, cleanset_id)
         label_column = api.get_label_column_of_project(self._api_key, project_id)
@@ -73,23 +84,45 @@ class Studio:
         # XXX this does not handle excluded columns correctly, because the API
         # returns all rows regardless and doesn't let us distinguish between
         # excluded and non-excluded rows
-        new_labels = cl_cols_df.select([id_col, 'cleanlab_clean_label']).withColumnRenamed('cleanlab_clean_label', label_column)
-        corrected_df = dataset.drop(label_column).join(new_labels, on=id_col, how='right')
+        both = cl_cols_df.select([id_col, "cleanlab_clean_label"]).join(
+            dataset.select([id_col, label_column]),
+            on=id_col,
+            how="left",
+        )
+        final = both.withColumn(
+            "__cleanlab_final_label",
+            # XXX hacky, relies on no labels being "None" (the string)
+            # instead, use original JSON, which uses null values where it's not specified
+            udf(lambda original, clean: original if clean == "None" else clean)(
+                both[label_column],
+                "cleanlab_clean_label",
+            ),
+        )
+        new_labels = final.select([id_col, "__cleanlab_final_label"]).withColumnRenamed(
+            "__cleanlab_final_label", label_column
+        )
+        corrected_df = dataset.drop(label_column).join(new_labels, on=id_col, how="right")
         return corrected_df
-
 
     def download_cleanlab_columns(self, cleanset_id: str) -> pd.DataFrame:
         project_id = api.get_project_of_cleanset(self._api_key, cleanset_id)
         label_column = api.get_label_column_of_project(self._api_key, project_id)
         return self._download_cleanlab_columns(cleanset_id, project_id, label_column)
 
-
-    def _download_cleanlab_columns(self, cleanset_id: str, project_id: str, label_column: str) -> pd.DataFrame:
+    def _download_cleanlab_columns(
+        self, cleanset_id: str, project_id: str, label_column: str
+    ) -> pd.DataFrame:
         rows = api.download_cleanlab_columns(self._api_key, cleanset_id, all=True)
         id_col = api.get_id_column(self._api_key, cleanset_id)
         # TODO actually get _all_ the columns incl e.g., cleanlab_top_labels
         # and have the API give the column headers rather than this library hard-coding it
-        headers = [id_col, "cleanlab_issue", "cleanlab_label_quality", "cleanlab_suggested_label", "cleanlab_clean_label"]
+        headers = [
+            id_col,
+            "cleanlab_issue",
+            "cleanlab_label_quality",
+            "cleanlab_suggested_label",
+            "cleanlab_clean_label",
+        ]
         dataset_id = api.get_dataset_of_project(self._api_key, project_id)
         schema = api.get_dataset_schema(self._api_key, dataset_id)
         col_types = {
@@ -101,5 +134,10 @@ class Studio:
         }
 
         # convert to dict/column-major format
-        d = {headers[j]: np.array([rows[i][j] for i in range(len(rows))], dtype=col_types[headers[j]]) for j in range(len(headers))}
+        d = {
+            headers[j]: np.array(
+                [rows[i][j] for i in range(len(rows))], dtype=col_types[headers[j]]
+            )
+            for j in range(len(headers))
+        }
         return pd.DataFrame(d)
