eda5bb5c86f5a73f2231c763d0e7c8b88529ffe8
ryansingman <ryanjs@vt.edu>
2022-12-27 14:45:12 -0700

    subclass InvalidDatasetError from ValueError, pass instead of elipsis

--- cleanlab_studio/errors.py
+++ cleanlab_studio/errors.py
@@ -1,10 +1,10 @@
-class InvalidDatasetError(RuntimeError):
-    ...
+class InvalidDatasetError(ValueError):
+    pass
 
 
 class EmptyDatasetError(InvalidDatasetError):
-    ...
+    pass
 
 
 class ColumnMismatchError(InvalidDatasetError):
-    ...
+    pass
