e45fe1d5ab575cfe9aa3e02eb0bf3c6fa2939250
Angela <angela@cleanlab.ai>
2023-05-01 09:55:23 -0700

    fix init_dataset_source

--- cleanlab_studio/internal/util.py
+++ cleanlab_studio/internal/util.py
@@ -30,21 +30,26 @@ def init_dataset_source(
         if dataset_name is None:
             raise ValueError("Must provide dataset name if uploading from a DataFrame")
         return PandasDatasetSource(df=dataset_source, dataset_name=dataset_name)
-    elif isinstance(dataset_source, pyspark.sql.DataFrame):
-        if not pyspark_exists:
-            raise ImportError(
-                'Must install pyspark to upload from pyspark dataframe. Use "pip install pyspark"'
-            )
-
-        from .dataset_source import PySparkDatasetSource
-
-        if dataset_name is None:
-            raise ValueError("Must provide dataset name if uploading from a DataFrame")
-        return PySparkDatasetSource(df=dataset_source, dataset_name=dataset_name)
     elif isinstance(dataset_source, pathlib.Path):
         return FilepathDatasetSource(filepath=dataset_source, dataset_name=dataset_name)
     elif isinstance(dataset_source, str):
         return FilepathDatasetSource(
             filepath=pathlib.Path(dataset_source), dataset_name=dataset_name
         )
-    raise ValueError("Invalid dataset source provided")
+    else:
+        try:
+            if isinstance(dataset_source, pyspark.sql.DataFrame):
+                if not pyspark_exists:
+                    raise ImportError(
+                        'Must install pyspark to upload from pyspark dataframe. Use "pip install pyspark"'
+                    )
+
+                from .dataset_source import PySparkDatasetSource
+
+                if dataset_name is None:
+                    raise ValueError("Must provide dataset name if uploading from a DataFrame")
+                return PySparkDatasetSource(df=dataset_source, dataset_name=dataset_name)
+        except NameError:
+            pass
+        finally:
+            raise ValueError("Invalid dataset source provided")
