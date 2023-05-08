b1c508b020148969bb8123ee6fd17896585bc419
Angela Liu <122922961+axl1313@users.noreply.github.com>
2023-05-08 11:13:42 -0700

    backwards compatible change to support better simple zip schema handling (#91)
    temp change to support better simple zip schema handling w backwards compatibility

--- cleanlab_studio/studio/upload.py
+++ cleanlab_studio/studio/upload.py
@@ -21,14 +21,14 @@ def upload_dataset(
     upload_id = upload_dataset_file(api_key, dataset_source)
     schema = get_proposed_schema(api_key, upload_id)
 
-    if schema is None and (
+    if (schema is None or schema.get("immutable", False)) and (
         schema_overrides is not None or modality is not None or id_column is not None
     ):
         print(
             "Warning: schema_overrides, modality, and id_column parameters will be ignored for simple zip uploads"
         )
 
-    if schema is not None:
+    if schema is not None and not schema.get("immutable", False):
         schema["metadata"]["name"] = dataset_source.dataset_name
         if schema_overrides is not None:
             for field in schema_overrides:
