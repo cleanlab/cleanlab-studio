cc2076199388232863a8fc308550287056a392af
Angela Liu <122922961+axl1313@users.noreply.github.com>
2023-07-19 10:52:02 -0600

    Fix: guess filetype from filename instead of dataset name (#116)
    guess filetype from filename instead of dataset name

--- cleanlab_studio/internal/dataset_source/filepath_dataset_source.py
+++ cleanlab_studio/internal/dataset_source/filepath_dataset_source.py
@@ -7,12 +7,16 @@ from .dataset_source import DatasetSource
 
 class FilepathDatasetSource(DatasetSource):
     def __init__(
-        self, *args: Any, filepath: pathlib.Path, dataset_name: Optional[str] = None, **kwargs: Any
+        self,
+        *args: Any,
+        filepath: pathlib.Path,
+        dataset_name: Optional[str] = None,
+        **kwargs: Any,
     ):
         super().__init__(*args, **kwargs)
         self.dataset_name = dataset_name if dataset_name is not None else filepath.name
         self.file_size = filepath.stat().st_size
-        maybe_file_type = mimetypes.guess_type(self.dataset_name)[0]
+        maybe_file_type = mimetypes.guess_type(filepath)[0]
         if maybe_file_type is None:
             raise ValueError(
                 f"Could not identify type of file at {filepath}. Make sure file name has valid extension"
