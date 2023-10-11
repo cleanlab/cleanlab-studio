fdc9112185a1e3f015fdaf198e59ae6cd4585610
clu0 <chenlu96@gmail.com>
2023-10-10 22:57:05 -0400

    change default upload df type to json

--- cleanlab_studio/internal/dataset_source/pandas_dataset_source.py
+++ cleanlab_studio/internal/dataset_source/pandas_dataset_source.py
@@ -8,6 +8,6 @@ from .dataframe_dataset_source import DataFrameDatasetSource
 class PandasDatasetSource(DataFrameDatasetSource[pd.DataFrame]):
     def _init_fileobj_from_df(self, df: pd.DataFrame) -> IO[bytes]:
         fileobj = io.BytesIO()
-        df.to_csv(fileobj, index=False)
+        df.to_json(fileobj, orient="records")
         fileobj.seek(0)
         return fileobj
