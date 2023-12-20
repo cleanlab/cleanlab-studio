852cbb7f77e21d13ab89a0b3b2fd026d9a85aec9
Animator Joe <josephjin7@gmail.com>
2023-12-20 17:44:44 -0600

    refactoring

--- cleanlab_studio/__init__.py
+++ cleanlab_studio/__init__.py
@@ -1,8 +1,3 @@
 from .version import __version__ as __version__
 
 from .studio.studio import Studio
-from .studio.snowflake_utils import (
-    get_snowflake_datarows,
-    get_snowflake_simple_imageset,
-    get_snowflake_metadata_imageset,
-)
