76c848cd2f8ed6d1e246e65010f093d491b80506
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2024-03-07 00:29:03 -0500

    constants

--- cleanlab_studio/internal/constants.py
+++ cleanlab_studio/internal/constants.py
@@ -4,9 +4,8 @@ from typing import List, Tuple
 # prepend constants with _ so that they don't show up in help.cleanlab.ai docs
 _DEFAULT_MAX_CONCURRENT_TLM_REQUESTS: int = 16
 _VALID_TLM_QUALITY_PRESETS: List[str] = ["best", "high", "medium", "low", "base"]
-
 _VALID_TLM_MODELS: List[str] = ["gpt-3.5-turbo-16k", "gpt-4"]
+_TLM_MAX_RETRIES: int = 10  # TODO: finalize this number
 TLM_MAX_TOKEN_RANGE: Tuple[int, int] = (64, 512)  # (min, max)
 TLM_NUM_CANDIDATE_RESPONSES_RANGE: Tuple[int, int] = (1, 100)  # (min, max)
 TLM_NUM_CONSISTENCY_SAMPLES_RANGE: Tuple[int, int] = (0, 50)  # (min, max)
-_TLM_MAX_RETRIES: int = 10  # TODO: finalize this number
