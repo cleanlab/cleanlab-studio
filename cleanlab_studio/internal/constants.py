081ef2cc3fab6a20293fe8ce363ded48a4c2823f
Hui Wen <45724323+huiwengoh@users.noreply.github.com>
2024-03-28 14:16:19 -0400

    increase max_retries

--- cleanlab_studio/internal/constants.py
+++ cleanlab_studio/internal/constants.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 # prepend constants with _ so that they don't show up in help.cleanlab.ai docs
 _VALID_TLM_QUALITY_PRESETS: List[str] = ["best", "high", "medium", "low", "base"]
 _VALID_TLM_MODELS: List[str] = ["gpt-3.5-turbo-16k", "gpt-4"]
-_TLM_MAX_RETRIES: int = 1  # TODO: finalize this number
+_TLM_MAX_RETRIES: int = 3  # TODO: finalize this number
 TLM_MAX_TOKEN_RANGE: Tuple[int, int] = (64, 512)  # (min, max)
 TLM_NUM_CANDIDATE_RESPONSES_RANGE: Tuple[int, int] = (1, 20)  # (min, max)
 TLM_NUM_CONSISTENCY_SAMPLES_RANGE: Tuple[int, int] = (0, 20)  # (min, max)
