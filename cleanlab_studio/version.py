16074dfa24de26ccb39e74fbca5b76c46c5a7a85
Hui Wen <45724323+huiwengoh@users.noreply.github.com>
2024-04-19 14:51:37 -0400

    Bump version (2.0.2) (#219)

--- cleanlab_studio/version.py
+++ cleanlab_studio/version.py
@@ -1,7 +1,7 @@
 # Note to developers:
 # Consider if backend's MIN_CLI_VERSION needs updating when pushing any changes to this file.
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 SCHEMA_VERSION = "0.2.0"
 MIN_SCHEMA_VERSION = "0.1.0"
