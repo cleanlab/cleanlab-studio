ac1f04b17fd278c21ac5b090c9c7fd1f5e95b69d
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2024-05-23 17:30:06 -0400

    add claude models

--- cleanlab_studio/internal/constants.py
+++ cleanlab_studio/internal/constants.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 # TLM constants
 # prepend constants with _ so that they don't show up in help.cleanlab.ai docs
 _VALID_TLM_QUALITY_PRESETS: List[str] = ["best", "high", "medium", "low", "base"]
-_VALID_TLM_MODELS: List[str] = ["gpt-3.5-turbo-16k", "gpt-4"]
+_VALID_TLM_MODELS: List[str] = ["gpt-3.5-turbo-16k", "gpt-4", "claude-3-haiku", "claude-3-sonnet"]
 _TLM_MAX_RETRIES: int = 3  # TODO: finalize this number
 TLM_MAX_TOKEN_RANGE: Tuple[int, int] = (64, 512)  # (min, max)
 TLM_NUM_CANDIDATE_RESPONSES_RANGE: Tuple[int, int] = (1, 20)  # (min, max)
