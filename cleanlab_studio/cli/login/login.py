a92d319bd209729c02bdcc42aacddb82f6917379
Kathryn (Kat) Wicks <wicksk@bxscience.edu>
2024-06-14 15:22:30 -0700

    black

--- cleanlab_studio/cli/login/login.py
+++ cleanlab_studio/cli/login/login.py
@@ -26,7 +26,9 @@ def login(prev_state: PreviousState, key: str) -> None:
     # validate API key
     valid_key = validate_api_key(key)
     if not valid_key:
-        raise AuthError("API key is invalid. Check https://app.cleanlab.ai/upload for your current API key.")
+        raise AuthError(
+            "API key is invalid. Check https://app.cleanlab.ai/upload for your current API key."
+        )
 
     # save API key
     try:
