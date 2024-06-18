0a68bc634d593c94e6c76fc46a788e66250a8dc3
Kathryn (Kat) Wicks <wicksk@bxscience.edu>
2024-06-17 17:45:52 -0700

    use old mypy syntax

--- cleanlab_studio/errors.py
+++ cleanlab_studio/errors.py
@@ -1,5 +1,6 @@
 from asyncio import Handle
 import pathlib
+from mypy import Union
 
 
 class HandledError(Exception):
@@ -141,7 +142,7 @@ class InvalidProjectConfiguration(HandledError):
 
 
 class InvalidFilepathError(HandledError):
-    def __init__(self, filepath: str | pathlib.Path = "") -> None:
+    def __init__(self, filepath: Union[str, pathlib.Path] = "") -> None:
         if isinstance(filepath, pathlib.Path):
             filepath = str(filepath)
         super().__init__(f"File could not be found at {filepath}. Please check the file path.")
