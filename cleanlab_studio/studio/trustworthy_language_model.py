fa5b999584da3529c211b05515150105fce126e1
Jay Zhang <jay@Jays-MacBook-Pro.local>
2024-07-15 11:10:46 -0700

    format

--- cleanlab_studio/studio/trustworthy_language_model.py
+++ cleanlab_studio/studio/trustworthy_language_model.py
@@ -61,7 +61,7 @@ class TLM:
 
         Args:
             api_key: You can find your API key on your [account page](https://app.cleanlab.ai/account) in Cleanlab Studio. Instead of specifying the API key here, you can also log in with `cleanlab login` on the command-line.
-            
+
             quality_preset (TLMQualityPreset): An optional preset to control the quality of TLM responses and trustworthiness scores vs. runtimes/costs.
                 TLMQualityPreset is a string specifying one of the supported presets, including "best", "high", "medium", "low", "base".
 
