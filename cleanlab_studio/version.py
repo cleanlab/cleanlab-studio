5613027e769edf6ecf43aeaef65c20b3ef33cfa4
Jay Zhang <jas2600@gmail.com>
2024-07-17 21:58:59 -0700

    bump version to 2.1.3

--- cleanlab_studio/version.py
+++ cleanlab_studio/version.py
@@ -1,7 +1,7 @@
 # Note to developers:
 # Consider if backend's MIN_CLI_VERSION needs updating when pushing any changes to this file.
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 
 SCHEMA_VERSION = "0.2.0"
 MIN_SCHEMA_VERSION = "0.1.0"
