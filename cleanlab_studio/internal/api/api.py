43fe2e92f700ead23e484d85c5b0aed3b11519b7
Aaron Zhang <awesome1aaron@gmail.com>
2024-07-31 12:54:17 -0700

    update notebook

--- cleanlab_studio/internal/api/api.py
+++ cleanlab_studio/internal/api/api.py
@@ -43,7 +43,7 @@ from cleanlab_studio.internal.api.api_helper import check_uuid_well_formed
 from cleanlab_studio.internal.types import JSONDict, SchemaOverride, TLMQualityPreset
 from cleanlab_studio.version import __version__
 
-base_url = os.environ.get("CLEANLAB_API_BASE_URL", "https://api.dev-bc26qf4m.cleanlab.ai/api")
+base_url = os.environ.get("CLEANLAB_API_BASE_URL", "https://api.cleanlab.ai/api")
 cli_base_url = f"{base_url}/cli/v0"
 upload_base_url = f"{base_url}/upload/v1"
 dataset_base_url = f"{base_url}/datasets"
