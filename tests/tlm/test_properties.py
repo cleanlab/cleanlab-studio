b7fb5f06a138483f37cdbbe21a0ba5ce623641d7
Ulyana <ulyana@cleanlab.ai>
2024-09-10 16:20:33 -0700

    remove not testing base get trustuworthiness score

--- tests/tlm/test_properties.py
+++ tests/tlm/test_properties.py
@@ -12,9 +12,6 @@ from tests.tlm.test_get_trustworthiness_score import is_trustworthiness_score
 from tests.tlm.test_prompt import is_tlm_response
 
 _VALID_TLM_MODELS = ["gpt-4o-mini", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4o", "claude-3-haiku"]
-_VALID_TLM_QUALITY_PRESETS_WITHOUT_BASE = [
-    quality_preset for quality_preset in _VALID_TLM_QUALITY_PRESETS if quality_preset != "base"
-]
 
 
 def _test_log(response: Dict[str, Any], options: Dict[str, Any]) -> None:
@@ -140,7 +137,7 @@ def test_try_prompt(tlm_dict: Dict[str, Any], model: str, quality_preset: str) -
 
 
 @pytest.mark.parametrize("model", _VALID_TLM_MODELS)
-@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS_WITHOUT_BASE)
+@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS)
 def test_get_trustworthiness_score(
     tlm_dict: Dict[str, Any], model: str, quality_preset: str
 ) -> None:
@@ -162,7 +159,7 @@ def test_get_trustworthiness_score(
 
 
 @pytest.mark.parametrize("model", _VALID_TLM_MODELS)
-@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS_WITHOUT_BASE)
+@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS)
 def test_get_trustworthiness_score_async(
     tlm_dict: Dict[str, Any], model: str, quality_preset: str
 ) -> None:
@@ -190,7 +187,7 @@ def test_get_trustworthiness_score_async(
 
 
 @pytest.mark.parametrize("model", _VALID_TLM_MODELS)
-@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS_WITHOUT_BASE)
+@pytest.mark.parametrize("quality_preset", _VALID_TLM_QUALITY_PRESETS)
 def test_try_get_trustworithness_score(
     tlm_dict: Dict[str, Any], model: str, quality_preset: str
 ) -> None:
