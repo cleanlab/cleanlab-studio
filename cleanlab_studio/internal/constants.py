d9b66a8883c5c3b833188af8e602b168ca524c16
Ulyana <ulyana@cleanlab.ai>
2024-09-19 15:30:49 -0700

    update max tokens range

--- cleanlab_studio/internal/constants.py
+++ cleanlab_studio/internal/constants.py
@@ -14,7 +14,7 @@ _VALID_TLM_MODELS: List[str] = [
 ]
 _TLM_DEFAULT_MODEL: str = "gpt-4o-mini"
 _TLM_MAX_RETRIES: int = 3  # TODO: finalize this number
-TLM_MAX_TOKEN_RANGE: Tuple[int, int] = (64, 512)  # (min, max)
+TLM_MAX_TOKEN_RANGE: Tuple[int, int] = (64, 4096)  # (min, max)
 TLM_NUM_CANDIDATE_RESPONSES_RANGE: Tuple[int, int] = (1, 20)  # (min, max)
 TLM_NUM_CONSISTENCY_SAMPLES_RANGE: Tuple[int, int] = (0, 20)  # (min, max)
 TLM_VALID_LOG_OPTIONS: Set[str] = {"perplexity", "explanation"}
